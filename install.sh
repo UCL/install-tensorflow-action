1a22950aff4c89d1d3bcca66d68e836832ae4cf6
Sarah Jaffa <SJaffa@users.noreply.github.com>
2022-12-14 15:38:27 +0000

    Add option for different operating systems

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@
 
 set -e
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     version="2.9.1"
+    os="linux"
+else if [ "$#" -lt 2 ]; then
+    version="$1"
+    os="linux"
 else
     version="$1"
+    os="$2"
 fi
-FILENAME="libtensorflow-cpu-linux-x86_64-$version.tar.gz"
+
+FILENAME="libtensorflow-cpu-$os-x86_64-$version.tar.gz"
 echo "Using $FILENAME"
 wget -q --no-check-certificate https://storage.googleapis.com/tensorflow/libtensorflow/${FILENAME}
 sudo tar -C /usr/local -xzf ${FILENAME}
